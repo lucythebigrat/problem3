64201f1dee997a1838b66521301c8586ea093cbd
lucytherat <lucythebigrat@gmail.com>
2024-10-28 22:42:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#make a function that will find the largest number in a list
+
+#psueodcode
+
+# list = [things in list]
+# function(list you want to check):
+# print(max(list you want to check))
+# function(list)
+
+
+list = [1, 2903812312321, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def findMax(ListThatYouWantToCheck):
+    print(f"The maximum number of the list that you want to check is: {max(ListThatYouWantToCheck)}")
+
+findMax(list)
